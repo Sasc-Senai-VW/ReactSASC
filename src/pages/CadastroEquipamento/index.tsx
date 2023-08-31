16328ec5e603c11693df7534ce115092350bfaba
Janaina Mota <janaother16@gmail.com>
2023-08-30 21:54:35 -0300

    paginaCadastroEquipamentos

--- src/pages/CadastroEquipamento/index.tsx
+++ src/pages/CadastroEquipamento/index.tsx
@@ -1,9 +1,125 @@
 import "./style.css";
 
+import { BsPcDisplay } from "react-icons/bs";
+import { BsArrowDownShort } from "react-icons/bs";
+
+
 export default function CadastroEquipamento() {
   return (
     <>
-      <h1>CadastroEquipamento</h1>
+     <main>   
+        <h1>Novos equipamentos</h1>     
+
+        <h2> Cadastrar novos equipamentos </h2> 
+
+      <div className="cadastro_de_equipamentos">
+        <form>
+
+
+          <div className="inputs">
+          <div className="input-with-icon">
+
+            <input placeholder="Modelo"  
+            type="text" 
+            id="campo-modelo" /> 
+
+            <input placeholder="Categoria"
+             type="text" 
+             id="campo-categoria" />
+
+            <input
+              placeholder="Número de Série"
+              type="text"
+              id="campo-numero_de_serie"
+            />
+            
+                <BsPcDisplay className="modelo-icon"/>
+                <BsArrowDownShort className="seta-icon"/>
+
+                
+            
+          </div>
+          </div>
+
+         
+
+
+          <div className="inputs">
+            <input placeholder="Fabricante" type="text" id="campo-fabricante" />
+            <input placeholder="Consumo" type="text" id="campo-consumo" />
+            <input placeholder="Setor" type="text" id="campo-setor" />
+          </div>
+          <div className="inputs3">
+            <input
+              placeholder="Ano de Fabricação"
+              type="text"
+              id="campo-ano_de_fabricação"
+            />
+            <input placeholder="Preço" type="text" id="campo-preço" />
+            <button type="submit">Confirmar</button>
+          </div>
+        </form>
+      </div>
+      <div className="maquinas_cadastradas">
+        <h3>Máquinas Cadastradas</h3>
+        <div className="tabela">
+          <table>
+            <tbody>
+              <tr>
+                <th>Codigo:1</th>
+                <td>Modelo 1</td>
+                <td>Fabricante 1</td>
+                <td>Categoria 1</td>
+                <td>Setor 1</td>
+                <td>00/00/00</td>
+                <td>R$9,999</td>
+                <td>
+                  1,200 watts/Hr <button>Alterar</button>{" "}
+                </td>
+              </tr>
+              <tr>
+                <th>Codigo:2</th>
+                <td>Modelo 2</td>
+                <td>Fabricante 2</td>
+                <td>Categoria 2</td>
+                <td>Setor 2</td>
+                <td>00/00/00</td>
+                <td>R$9,999</td>
+                <td>
+                  1,200 watts/Hr <button>Alterar</button>{" "}
+                </td>
+              </tr>
+              <tr>
+                <th>Codigo:3</th>
+                <td>Modelo 3</td>
+                <td>Fabricante 3</td>
+                <td>Categoria 3</td>
+                <td>Setor 3</td>
+                <td>00/00/00</td>
+                <td>R$9,999</td>
+                <td>
+                  1,200 watts/Hr <button>Alterar</button>{" "}
+                </td>
+              </tr>
+              <tr>
+                <th>Codigo:4</th>
+                <td>Modelo 4</td>
+                <td>Fabricante 4</td>
+                <td>Categoria 4</td>
+                <td>Setor 4</td>
+                <td>00/00/00</td>
+                <td>R$9,999</td>
+                <td>
+                  1,200 watts/Hr <button>Alterar</button>{" "}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+   
+  </main>
+
     </>
   );
 }
