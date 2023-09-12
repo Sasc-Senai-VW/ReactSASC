1fee888f732bfc7ea3119ee96485b53f45b29194
t-rindade <tr.ndade3@gmail.com>
2023-09-11 21:14:40 -0300

    phome

--- src/main.tsx
+++ src/main.tsx
@@ -11,10 +11,14 @@ import Equipamentos from "./pages/Equipamentos";
 import EsqueceuSenha from "./pages/EsqueceuSenha";
 import Login from "./pages/Login";
 import NovoUsuario from "./pages/NovoUsuario";
+import Menu from "./components/Menu";
+import Header from "./components/Header";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Header />
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="cadastroequipamento" element={<CadastroEquipamento />} />
