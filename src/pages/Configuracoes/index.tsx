91cf6c32c1e0919c4a3f19f558149230cf3605fa
Janaina Mota <janaother16@gmail.com>
2023-09-11 21:49:47 -0300

    atualizacoes realizadas no index e css

--- src/pages/Configuracoes/index.tsx
+++ src/pages/Configuracoes/index.tsx
@@ -1,9 +1,86 @@
 import "./style.css";
 
-export default function Configuracoes() {
+import { useState } from "react";
+
+ function Configuracoes() {
+
+  const [categoria, Setcategoria] = useState<string[]>(
+    [
+        "Administrador",
+        "Comum"
+    ]
+  );
+
+  const [select, setSelect] = useState<string>(""); 
+
+
   return (
+
+    
+
     <>
-      <h1>Configuracoes</h1>
+
+        <main className="main_configuracoes">   
+
+        <h1>Configurações</h1>     
+
+        <h2>Configurações de Perfil</h2> 
+
+
+
+        <div className="inputs"> 
+
+             <input placeholder="Usuario"  
+             type="text" 
+             id="campo-usuario" /> 
+
+             <select
+                 name=""
+                 id="select-user"
+                 onChange={(e) => setSelect(e.target.value)}
+                 >
+                <option selected disabled value="">Categoria</option>
+                {
+                  categoria.map((tech: any, index: number) => {
+                  return <option key={index} value={tech}>{tech}</option>
+                 })
+                }
+              </select>
+
+              <input placeholder="Senha"  
+             type="password" 
+             id="campo-senha" /> 
+
+             <input placeholder="Confirmar senha"  
+             type="password" 
+             id="campo-confirmar-senha" />          
+
+            </div>
+
+            <button className="button1"
+              type="submit">
+              Confirmar
+              </button>
+
+              <button className="button2"
+              type="submit">
+              Cancelar
+              </button>
+
+
+
+
+       
+
+        
+        </main>
+
+
+
+    
+      
     </>
   );
 }
+
+export default Configuracoes
