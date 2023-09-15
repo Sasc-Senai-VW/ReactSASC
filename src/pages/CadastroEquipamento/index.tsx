a22e0e2ac6c5c904caad92510c3f3cc01c5e8b65
Janaina Mota <janaother16@gmail.com>
2023-09-15 20:18:52 -0300

    teste

--- src/pages/CadastroEquipamento/index.tsx
+++ src/pages/CadastroEquipamento/index.tsx
@@ -2,208 +2,156 @@ import "./style.css";
 
 import { useEffect, useState } from "react";
 
-import { BsPersonCircle } from "react-icons/bs";
-
 import axios from "axios";
 
-import api from "../../api";
+import api from "../../utils/api";
 
 
+function CadastroEquipamento() {
 
-   function CadastroEquipamento() {
+  const [categorias, setCategorias] = useState<string[]>(
+    [
+        "Categoria1",
+        "Categoria2",
+        "Categoria3"
+    ]
+);
+  const [select, setSelect] = useState<string>("");
 
-    const [categorias, setCategorias] = useState<string[]>(
-      [
-          "Categoria1",
-          "Categoria2",
-          "Categoria3"
-      ]
-      );
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState<string[]>([]); 
 
-   const [ modelo, setModelo] = useState<string>("")
-   const [ fabricante, setFabricante ] = useState<string>("")
-   const [ ano, setAno ] = useState<string>("")
-   const [ setor, setSetor] = useState<string>("")
-   const [ consumo, setConsumo ] = useState<string>("")
-   const [ preco, setPreco ] = useState<string>("")
-   const [ serie, setSerie ] = useState<string>("")
-  
-   const [select, setSelect] = useState<string>(""); //equipamento selecionado
-  
- 
-    function cadastrarEquipamento (event: any) {
-      event.prevent.default();
 
-     const formdata = new FormData ()
+  const [id, setId] = useState<string>("");
+  const [modelo, setModelo] = useState<string>("")
+  const [data, setData] = useState<string>("")
+  const [fabricante, setFabricante] = useState<string>("")
+  const [valor, setValor] = useState<string>("")
+  const [consumo, setConsumo] = useState<string>("")
 
-     formdata.append("modelo", modelo)
-     formdata.append("fabricante", fabricante)
-     formdata.append("ano", ano)
-     formdata.append("setor", setor)
-     formdata.append("consumo", consumo)
-     formdata.append("preco", preco)
-     formdata.append("serie", serie)
-   }
- }
-  
-  const [equipamentos, setEquipamentos] = useState([]);
+  function cadastrarEquipamentos(event: any) {
 
-  const handleSubmit = async (event: any) => {
-    event.preventDefault();
+    event.preventDefault(); //é para manter as informações no campo, após o submit
 
-  const formData = new FormData(event.target);
+    const formData = new FormData() //trata arquivos de forma intuitiva, no caso de imagens
+
+    formData.append("id", id) //tudo que for append é adicionar
+    formData.append("modelo", modelo)
+    formData.append("data", data)
+    formData.append("fabricante", fabricante)
+    formData.append("valor", valor)
+    formData.append("consumo", consumo)
+
+    formData.append("categoria", JSON.stringify(setCategoriaSelecionada))
+
+    api.post("http://localhost:3000", formData).then((response) => { 
+      console.log(response)
+      alert("Equipamento criado com sucesso! ")
+
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 
-  const [categoriaSelecionadas, setCategoriaSelecionadas] = useState<string[]>([]); 
-
-  const [select, setSelect] = useState<string>(""); 
-
-
-  function adicionarCategoria() {
-   
-    if (select === "") {
-      
-        alert("Selecione uma categoria para adicionar");
-    } else {
-       
-        if (categoriaSelecionadas.includes(select)) {
-            
-            alert("Essa categoria já foi selecionada");
-        }
-        else {
-
-            let novaCategoriaSelecionadas = [...categoriaSelecionadas];
-
-            novaCategoriaSelecionadas.push(select);
-
-           
-            setCategoriaSelecionadas(novaCategoriaSelecionadas);
-        }
-    }
-}
+  
+ 
 
 
-  return (
-    <>
 
 
-     <main className="main_cadastro">   
-        <h1>Novos equipamentos</h1>     
+return (
+  <>
 
-        <h2> Cadastrar novos equipamentos </h2> 
+
+    <main className="main_cadastro">
+      <h1>Novos equipamentos</h1>
+
+      <h2> Cadastrar novos equipamentos </h2>
 
       <div className="cadastro_de_equipamentos">
-        <form onSubmit={cadastrarEquipamento} className="cad_formulario"  method="POST">
+        <form onSubmit={cadastrarEquipamentos} className="cad_formulario" method="POST">
 
-            <div className="inputs1"> 
+          <div className="inputs1">
 
-             <input placeholder="Modelo"  
-             type="text" 
-             onChange={ (event) => { setModelo(event.target.value) } }
-             id="campo-modelo" /> 
-
-             <select
-                 name=""
-                 id="select-categoria"
-                 onChange={(e) => setSelect(e.target.value)}
-                 >
-                <option selected disabled value="">Categoria</option>
-                {
-                  categoria.map((categoria: any, index: number) => {
-                  return <option key={index} value={categoria}>{categoria}</option>
-                 })
-                }
-              </select>
-
-              <input
-              placeholder="Número de Série"
+            <input placeholder="ID"
               type="text"
-              onChange={ (event) => { set(event.target.value) } }
-              id="campo-numero_de_serie" />
+              onChange={(event) => { setId(event.target.value) }}
+              id="campo-ID" />
 
-            </div> 
-
-
-            <div className="inputs2">
-              
-              
-             <input placeholder="Fabricante" 
-             type="text" 
-             id="campo-fabricante" />
-
-              <input placeholder="Consumo" 
-              type="text" 
-              id="campo-consumo" />
-
-              <input placeholder="Setor" 
-              type="text" 
-              id="campo-setor" />
-
-            </div>
+            <select
+              name=""
+              id="select-categoria"
+              onChange={(e) => setSelect(e.target.value)}
+            >
+              <option selected disabled value="">Categoria</option>
+              {
+                categorias.map((categoria: any, index: number) => {
+                  return <option key={index} value={categoria}>{categoria}</option>
+                })
+              }
+            </select>
 
            
+          </div>
+
+
+          <div className="inputs2">
+
+
+            <input placeholder="Fabricante"
+              type="text"
+              onChange={(event) => { setFabricante(event.target.value) }}
+              id="campo-fabricante" />
+
+            <input placeholder="Data"
+              type="text"
+              onChange={(event) => { setData(event.target.value) }}
+              id="campo-data" />
+
             
+
+          </div>
+
+
+
 
           <div className="inputs3">
 
-            <input
-              placeholder="Ano de Fabricação"
+          <input placeholder="Consumo"
               type="text"
-              id="campo-ano_de_fabricação" />
+              onChange={(event) => { setConsumo(event.target.value) }}
+              id="campo-consumo" />
 
-              <input placeholder="Preço" 
-              type="text" 
-              id="campo-preço" />
+               <input
+              placeholder="modelo"
+              type="text"
+              onChange={(event) => { setModelo(event.target.value) }}
+              id="campo-modelo" />
+          </div>
 
-              <button 
+          
+            <button
               type="submit">
               Confirmar
-              </button>
-
-          </div>
+            </button>
         </form>
       </div>
-      
+
       <div className="maquinas_cadastradas">
         <h3>Máquinas Cadastradas</h3>
         <div className="tabela">
           <table>
 
-          <thead>
-          <tr>
-           
-            <th>Modelo</th>
-            <th>Fabricante</th>
-            <th>Setor</th>
-            <th>Data</th>
-            <th>Valor</th>
-            <th>Watts</th>
-            <tbody>
-          {equipamentos.map((equipamento: any) => (
-            <tr key={equipamento.modelo}>
-              <td>{equipamento.fabricante}</td>
-              <td>{equipamento.setor}</td>
-              <td>{equipamento.data}</td>
-              <td>{equipamento.valor}</td>
-              <td>{equipamento.watts}</td>
-                <button onClick={() => (equipamento.id)}>
-                  Editar
-                </button>
-              </tr>
-            
-          ))}
-        </tbody>
 
-        </thead>
-            
+
           </table>
         </div>
       </div>
-   
+
     </main>
 
-    </>
-  );
+  </>
+);
 }
 
-export default CadastroEquipamento 
+
+export default CadastroEquipamento
