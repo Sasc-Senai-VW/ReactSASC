b5797b2e0a3bd1cad2e6fd4c1f3d3531e32c16b3
Denis luan Sales de Santana <394.299.878-52@info.local>
2023-09-15 22:00:00 -0300

    ok

--- src/pages/Equipamento/index.tsx
+++ src/pages/Equipamento/index.tsx
@@ -2,9 +2,10 @@ import CardEquipamento from "../../components/CardEquip";
 import api from "../../utils/api";
 import { useEffect, useState } from "react"
 import './style.css'
+import search from '../../assets/img/search.png'
 
 
-export default Equipamento;   
+export default Equipamento;
 
 function Equipamento() {
   const [equip, setEquip] = useState<any[]>([])
@@ -16,12 +17,13 @@ function Equipamento() {
     [
       "ID",
       "MODELO",
-      "FABRICANTE",
+      'VALOR',
       'DATA',
       "CONSUMO",
-      'VALOR'
+      "FABRICANTE"
+
     ]);
-    
+
   const [select, setSelect] = useState<string>(""); // state que contém a opção de filtro selecionado pelo usuário
 
   const [filtroDigitado, setfiltroDigitado] = useState<string>("");
@@ -37,32 +39,38 @@ function Equipamento() {
 
   function buscarPor(event: any) {
     event.preventDefault();
-    
+
+    console.log(select)
+    console.log(equip)
+    console.log (filtroDigitado)
+
     let equipFiltrados = [];
 
     if (select == 'ID') {
-        equipFiltrados = equip.filter((eq: any) => eq.id.toString().includes(filtroDigitado.toLocaleUpperCase()))
-    } 
+      equipFiltrados = equip.filter((eq: any) => eq.id.toString().includes(filtroDigitado.toLocaleUpperCase()))
+    }
     else if (select == 'MODELO') {
       equipFiltrados = equip.filter((eq: any) => eq.modelo.includes(filtroDigitado.toLocaleUpperCase()))
-    } 
+    }
     else if (select == 'FABRICANTE') {
-      equipFiltrados = equip.filter((eq: any) => eq.fabricante.includes(filtroDigitado.toLocaleUpperCase()))
-    } 
+      equipFiltrados = equip.filter((eq: any) => eq.fabricante.toLocaleUpperCase().includes(filtroDigitado.toLocaleUpperCase()))
+    }
     else if (select == 'DATA') {
       equipFiltrados = equip.filter((eq: any) => eq.data.includes(filtroDigitado.toLocaleUpperCase()))
-    } 
+    }
     else if (select == 'CONSUMO') {
       equipFiltrados = equip.filter((eq: any) => eq.consumo.includes(filtroDigitado.toLocaleUpperCase()))
-    } 
+    }
     else if (select == 'VALOR') {
       equipFiltrados = equip.filter((eq: any) => eq.valor.includes(filtroDigitado.toLocaleUpperCase()))
     }
 
+    console.log(equipFiltrados)
+
     if (equipFiltrados.length === 0) {
       alert("Nenhum resultado encontrado!!")
     }
- 
+
     else {
       setlistaBuscaFiltrado(equipFiltrados)
     }
@@ -85,51 +93,55 @@ function Equipamento() {
 
   function alternarCoresTabela() {
     let linhas = document.getElementsByTagName("tr"); // Obtém todas as linhas da tabela
-  
-    for (let i = 1 ; i < linhas.length; i++) {
+
+    for (let i = 1; i < linhas.length; i++) {
       if (i % 2 === 0) { // Se o índice da linha for par
         linhas[i].style.backgroundColor = "#dceee8 "; // Define a cor de fundo como branco
       } else { // Se o índice da linha for ímpar
         linhas[i].style.backgroundColor = "#01b574"; // Define a cor de fundo como verde
       }
-  
+
       // Verifica se a linha possui a classe "tabelaEquip"
       if (linhas[i].classList.contains("tabelaEquip")) {
         linhas[i].style.backgroundColor = "white"; // usa a cor desejada
       }
     }
   }
-  
+
   alternarCoresTabela();
 
 
   return (
     <>
-      <main id="equip">
-        
+      <main>
+
         <section className="equipamentos">
-          
+
           <div className="painelEqpm">
 
             <form method="post" onSubmit={buscarPor}>
 
-            <select defaultValue={"DEFAULT"} name="" id="cad_select_skill" onChange={(e) => setSelect(e.target.value)}>
-              <option selected value="DEFAULT" disabled>Selecione</option>
-              {
-                filtros.map((equip: any, index: number) => {
-                  return <option key={index} value={equip}>{equip}</option>
-                })
-              }
-            </select>
+              <section className="table__header">
+                <h1>Lista de equipamentos</h1>
+                <select defaultValue={"DEFAULT"} name="" id="cad_select_skill" onChange={(e) => setSelect(e.target.value)}>
+                  <option selected value="DEFAULT" disabled>Selecione</option>
+                  {
+                    filtros.map((equip: any, index: number) => {
+                      return <option key={index} value={equip}>{equip}</option>
+                    })
+                  }
+                </select>
 
-            <input className="buscarInput"
-             type="search" 
-             placeholder="Buscar por filtros ..." 
-             onChange={retornoEquipGeral}
-             />
-            <button type="submit">Buscar</button>
+                <div className="input-group">
+                  <input type="search" placeholder="Procurar "
+                    onChange={retornoEquipGeral} />
+                  <button type='submit' ><img src={search} alt="" /></button>
+                </div>
+              </section>
 
-             </form>
+
+
+            </form>
 
             <div className="menu2Bbtn">
               <a
@@ -138,63 +150,58 @@ function Equipamento() {
               >
                 <button>+ Novo Equipamento</button>
               </a>
-              <a className="btnEditar" href="">
-                <button>Editar</button>
-              </a>
-              <a className="btnExcluir" href="">
-              <button>Excluir</button>
-              </a>
+
             </div>
             <table className="tabelaEqpm">
-              <thead className="linhaFiltro">
-                <tr className="linhaFiltro">
+              <thead>
+                <tr>
                   <th scope="col">Id</th>
                   <th scope="col">Modelo</th>
-                  <th scope="col">Fabricante</th>
+                  <th scope="col">Valor</th>
                   <th scope="col">Data</th>
                   <th scope="col">Consumo</th>
-                  <th scope="col">Valor</th>
+                  <th scope="col">Fabricante</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className= 'tabelaEquip'>
+                <tr className='tabelaEquip'>
                   {
                     listaBuscaFiltrado.length === 0 ?
                       equip.map((equip: any, index: number) => {
-                        return <td  key={index}>
+                        return <td key={index}>
 
-                            <CardEquipamento
+                          <CardEquipamento
 
-                              id={equip.id}
-                              modelo={equip.modelo}
-                              fabricante={equip.fabricante}
-                              data={equip.data}
-                              consumo={equip.consumo}
-                              valor={equip.valor}
-                              
-                              
-
-                            />
+                            id={equip.id}
+                            modelo={equip.modelo}
+                            valor={equip.valor}
+                            data={equip.data}
+                            consumo={equip.consumo}
+                            fabricante={equip.fabricante}
 
 
 
-                          </td>
-            }) : listaBuscaFiltrado.map((equip: any, index: number) => {
-                            return <td  key={index}>
+                          />
 
-                              <CardEquipamento
 
-                                id={equip.id}
-                                modelo={equip.modelo}
-                                fabricante={equip.fabricante}
-                                data={equip.data}
-                                consumo={equip.consumo}
-                                valor={equip.valor}
 
-                              />
-                            </td>
+                        </td>
+                      }) : listaBuscaFiltrado.map((equip: any, index: number) => {
+                        return <td key={index}>
 
-                          })
+                          <CardEquipamento
+
+                            id={equip.id}
+                            modelo={equip.modelo}
+                            valor={equip.valor}
+                            data={equip.data}
+                            consumo={equip.consumo}
+                            fabricante={equip.fabricante}
+
+                          />
+                        </td>
+
+                      })
                   }
                 </tr>
               </tbody>
